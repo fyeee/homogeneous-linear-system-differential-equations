5550cfdc0296f35a5e6a6416157d396d61dec7bf
fyeee <fengkai.ye@mail.utoronto.ca>
2020-02-05 00:38:53 -0500

    adding arrow to trajectory

--- ODEPlotter.py
+++ ODEPlotter.py
@@ -27,7 +27,8 @@ class ODEPlotter:
         y = odeint(self.dxdt, y0, t)
         x1 = y[:, 0]
         x2 = y[:, 1]
-        self.ax.plot(x1, x2)
+        line = self.ax.plot(x1, x2)[0]
+        self.add_arrow(line)
         self.set_limit()
 
     def plot_direction_field(self):
@@ -41,6 +42,26 @@ class ODEPlotter:
         DX2 /= norms
         self.ax.quiver(X1, X2, DX1, DX2, norms, pivot='mid', cmap=plt.cm.plasma)
         self.set_limit()
+
+    def add_arrow(self, line, position=None, size=25, color=None):
+        if color is None:
+            color = line.get_color()
+        unfiltered = np.vstack((line.get_xdata(), line.get_ydata())).T
+        filtered = unfiltered[(unfiltered[:, 0] >= -1 * X_MAX) & (unfiltered[:, 0] <= X_MAX) &
+                              (unfiltered[:, 1] >= -1 * Y_MAX) & (unfiltered[:, 1] <= Y_MAX)]
+        x_data = filtered[:, 0]
+        y_data = filtered[:, 1]
+        if position is None:
+            position = (min(x_data.max(), X_MAX) + max(x_data.min(), -1 * X_MAX)) / 2
+        # find closest index
+        start_ind = np.argmin(np.absolute(x_data - position))
+        end_ind = start_ind + 1
+        line.axes.annotate('',
+                           xytext=(x_data[start_ind], y_data[start_ind]),
+                           xy=(x_data[end_ind], y_data[end_ind]),
+                           arrowprops=dict(arrowstyle="->", color=color, lw=1.5),
+                           size=size
+                           )
 
     def setup_plot(self):
         self.ax.set_title('Trajectories and direction fields')
